31ed28caab2514c4766f7c6ae894ba279d071d3f
deftclaw <deftclaw@briskbear.rocks>
2023-07-14 03:46:54 -0600

    add 256-color checker, add ssh initializer

---
+++ ssh_init.sh
@@ -0,0 +1,12 @@
+function ssh_init() {
+  errColor="\u001b[38;5;160m"
+  warnColor="\u001b[38;5;47m"
+  nc="\u001b[0m"
+  sshHome=$HOME/.ssh
+
+  [[ -d $sshHome ]] \
+    && ( echo -e "$errColor$sshHome exists, skipping creation$nc" && return 0 ) \
+    || ( mkdir -pv $sshHome/config.d && echo 'Include config.d/*' > $sshHome/config )
+
+  printf "Host github\n  Hostname github.com\n  User git" > $sshHome/config.d/github
+}
