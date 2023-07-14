15920fcf9fbbbab962e0f3f30fbb08e4276d22ab
deftclaw <deftclaw@briskbear.rocks>
2023-07-14 03:46:54 -0600

    add 256-color checker, add ssh initializer

---
+++ color_sample.sh
@@ -0,0 +1,13 @@
+function color_sample() {
+  numbers=( $(seq 0 256) )
+  lead="\u001b[38;5;"
+  nc="\u001b[0m"
+
+  for c in ${numbers[@]}
+  do
+    printf "$lead${c}m$c$nc"
+    [[ $c -lt 10 ]] && printf " "
+    [[ $c -lt 100 ]] && printf " "
+    [[ $( echo "$c % 16"|bc ) -eq 0 ]] && printf "\n" || printf " "
+  done
+}
