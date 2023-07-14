23a270bbb9018d493d7116bd6874f0979a974d7d
deftclaw <deftclaw@briskbear.rocks>
2023-07-14 12:26:20 -0600

    add asdf_install_latest function, add gpg_ssh.sh

--- asdf.sh
+++ asdf.sh
@@ -1,3 +1,9 @@
-# Source essential asdf functions : 1689333659
+# Source essential asdf functions : 1689358340
 . /opt/asdf/asdf.sh
 . /opt/asdf/completions/asdf.bash
+
+function asdf_install_latest() {
+  last=$(asdf list-all $1|ag '^\d+\.\d+\.\d+$'|tail -n1)
+  asdf install $1 $last
+  asdf global $1 $last
+}
