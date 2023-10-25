5bdcef50c4385f573e06cf9822e060bc632b9ab1
Elijah <57646863+deftclaw@users.noreply.github.com>
2023-10-25 14:17:55 -0600

    Update asdf.sh
    Use grep instead of ag

--- asdf.sh
+++ asdf.sh
@@ -1,9 +1,10 @@
-# Source essential asdf functions : 1689358340
-. /opt/asdf/asdf.sh
-. /opt/asdf/completions/asdf.bash
+# Source essential asdf functions : 1698265058
+. /asdf/asdf.sh
+. /asdf/completions/asdf.bash
 
 function asdf_install_latest() {
-  last=$(asdf list-all $1|ag '^\d+\.\d+\.\d+$'|tail -n1)
+  [[ $(asdf plugin list|grep $1) ]] || asdf plugin-add $1
+  last=$(asdf list-all ruby|grep -x '[[:digit:]]\.[[:digit:]]\+\.[[:digit:]]\+'|tail -n1)
   asdf install $1 $last
   asdf global $1 $last
 }
