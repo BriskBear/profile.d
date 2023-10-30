5df727e6ec375c3accc7e7dbe23019d8c8fa24ee
deftclaw <eeccher@cppwind.com>
2023-10-30 12:30:42 -0600

    Use bash array and case statement to universalize package manager calls

--- package_manager.sh
+++ package_manager.sh
@@ -1,25 +1,53 @@
-# Manage Distribution-specific packages : 1693326791
-alias purge='sudo pacman -Rcc'
-alias query='sudo pacman -Q'
-alias setup='sudo pacman -S --noconfirm'
-alias update='sudo pacman -Syyu'
+# Call the appropriate package manager for your distro with unified verbiage : 1698689256
 
-clear_existing() {
-  declare -A rp_string
-  leaf=$1
+declare -A clean
+declare -A purge
+declare -A setup
+declare -A update 
+declare DISTRO
 
-  rp_string[0]='s/.\+: //g'
-  rp_string[1]='s/ exists in filesystem//g'
-  
-  exist=($(cat $leaf|sed "${rp_string[0]}"|sed "${rp_string[1]}"))
-  printf '%s\n' ${exist[@]}|xargs -I % sudo rm -rf %
+CLEAN["arch"]='pacman -Scc'
+PURGE["arch"]='pacman -Rcc'
+PURGE["ubuntu"]='apt purge'
+SETUP["arch"]='pacman -S --noconfirm'
+SETUP["ubuntu"]='apt-get install -y'
+UPDATE["arch"]='sudo pacman -Scc --noconfirm ; sudo pacman -Syu --noconfirm'
+UPDATE["ubuntu"]=ubuntu_update
+
+function ubuntu_update() {
+  sudo apt update
+  sudo apt upgrade -y
 }
 
-install_wiki() {
-  packages=($(echo "$@"|sed 's/\.//g'))
-  
-  setup ${packages[@]}
-  query ${packages[@]}
+function package() {
+  [[ -n $DISTRO ]] \
+    && ( export DISTRO=$( cat /etc/*{release,version} 2>/dev/null|grep -e '^ID='|awk -F= '{print $NF}' ))
+  unset -v call command
+
+  call=$(echo $1|tr '[:lower:]' '[:upper:]')
+  reply -i "call: $call"
+  case $call in
+    CLEAN)
+        command=${CLEAN[$DISTRO]}
+      ;;
+    PURGE)
+        command=${PURGE[$DISTRO]}
+      ;;
+    SETUP)
+        command=${SETUP[$DISTRO]}
+      ;;
+    UPDATE)
+        ${UPDATE[$DISTRO]}
+      ;;
+    *)
+      reply -w "Package currently only supports: <clean|purge|setup|update>"
+      ;;
+  esac
+
+  sudo $command ${*:2} 2>&1 >>/tmp/package_manager.log
 }
 
-alias Install='install_wiki'
+alias clean='package clean'
+alias purge='package purge'
+alias setup='package setup'
+alias update='package update'
