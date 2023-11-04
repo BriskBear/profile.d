992df00e815b419e3e224523d4d1c4ecd2cbddb2
Elijah <57646863+deftclaw@users.noreply.github.com>
2023-11-04 00:44:47 -0600

    Update asdf.sh
    back to opt

--- asdf.sh
+++ asdf.sh
@@ -1,7 +1,7 @@
 # Source essential asdf functions : 1698265058
 export ASDF_DIR=/asdf
-. /asdf/asdf.sh
-. /asdf/completions/asdf.bash
+. /opt/asdf/asdf.sh
+. /opt/asdf/completions/asdf.bash
 
 function asdf_install_latest() {
   [[ $(asdf plugin list|grep $1) ]] || asdf plugin-add $1
