ba11d380b56f655e964d995e3bc35430b7734c1c
Elijah <57646863+deftclaw@users.noreply.github.com>
2023-11-04 00:45:16 -0600

    Update asdf.sh
    support double-digit major versions

--- asdf.sh
+++ asdf.sh
@@ -5,7 +5,7 @@ export ASDF_DIR=/asdf
 
 function asdf_install_latest() {
   [[ $(asdf plugin list|grep $1) ]] || asdf plugin-add $1
-  last=$(asdf list-all $1|grep -x '[[:digit:]]\.[[:digit:]]\+\.[[:digit:]]\+'|tail -n1)
+  last=$(asdf list-all $1|grep -x '[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+'|tail -n1)
   asdf install $1 $last
   asdf global $1 $last
 }
