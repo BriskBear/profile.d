38dada00e376b3ba84a36a8fa1e6b44a05f04f69
Elijah <57646863+deftclaw@users.noreply.github.com>
2024-01-14 07:41:24 -0700

    Update new_function.sh
    Use the root user's config file

--- new_function.sh
+++ new_function.sh
@@ -1,8 +1,8 @@
 # Create a new built-in bash function : 1689333722
 function new-function() {
   [[ $# == 1 ]] \
-    && sudo nvim +3 /etc/profile.d/$1.sh \
-    || sudo nvim +5 /etc/profile.d/
+    && sudo -i nvim +3 /etc/profile.d/$1.sh \
+    || sudo -i nvim +5 /etc/profile.d/
 }
 
 alias nf='new-function'
