d3c66ad19e68b14e7bacbe8e528de61867835ef3
deftclaw <deftclaw@briskbear.rocks>
2024-01-20 08:04:42 -0700

    Remve editing-mode settings because neovim was complaining

--- shopt.sh
+++ shopt.sh
@@ -1,12 +1,6 @@
-# Global Default Bourne-Again Shell Behaviour : 1689333744
+# Global Default Bourne-Again Shell Behaviour : 1705762977
 shopt -s expand_aliases
 shopt -s checkjobs
 shopt -s checkwinsize
 shopt -s cmdhist
 shopt -s histappend
-set -o vi
-set editing-mode vi
-set keymap vi
-set show-mode-in-prompt on
-set vi-ins-mode-string \1\e[6 q\2
-set vi-cmd-mode-string \1\e[2 q\2
