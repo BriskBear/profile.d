46a9ae91f86d4f346cd51c2842dde054d5b2f29a
deftclaw <deftclaw@briskbear.rocks>
2024-01-29 21:30:33 -0700

    Update environment config and naming

--- envar.sh
+++
@@ -1,4 +0,0 @@
-# Setup default environment variables : 1689333699
-export EDITOR=/usr/bin/nvim
-export PAGER=/usr/bin/less
-export WEB=''
---
+++ environment.sh
@@ -0,0 +1,13 @@
+# Global Environment : 
+## Default Tools
+export ASDF_DIR=/opt/asdf
+export EDITOR=/usr/bin/nvim
+export PAGER=/usr/bin/bat
+
+## XDG Config
+export XDG_CONFIG_HOME=/etc/xdg
+export XDG_CONFIG_PROFILE=/etc/profile.d
+
+## XDG Shortcuts
+export XH=$XDG_CONFIG_HOME
+export XP=$XDG_CONFIG_PROFILE
