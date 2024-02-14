9ef5d9ceb1006d944738f21f8269744dcbba88aa
deftclaw <eeccher@cppwind.com>
2024-02-14 12:11:49 -0700

    Identify gpg-key by git siginingKey, example alias-style without including mine

--- clip_key.sh
+++ clip_key.sh
@@ -1,4 +1,4 @@
-# Decrypt keepass password to clipboard, clear after 5 seconds : 1696883656
+# Decrypt keepass password to clipboard, clear after 5 seconds : 
 function clipkey() {
   dunstify "ClipKey $1" -t 5000
 
@@ -22,8 +22,8 @@ function lock_key() {
     exit 1
   fi
   cred_dir=$HOME/.config/.cred
-  by_email=$(git config --global user.email)
-  default_key=$(gpg -K|ag $by_email|tail -n1|awk -F'\\(|\\)' '{print $2}')
+  by_signingKey=$(git config --global user.signingkey)
+  default_key=$(gpg -K --keyid-format=long|ag -B1 $by_signingKey|head -n1|awk -F'\\(|\\)' '{print $2}')
 
   reply "Encrypting with: $default_key"
 
@@ -33,12 +33,4 @@ function lock_key() {
     gpg -a -se -r $default_key -o $cred_dir/$1
 }
 
-alias 3d='clipkey 3d'
-alias bw='clipkey BitWarden'
-alias git_token='clipkey Git-Container'
-alias kp='clipkey KeePass'
-alias pi='clipkey Sentinal'
-alias pm='clipkey PassMark'
-alias qa='clipkey QuickAssist'
-alias ud='clipkey ud_admin.asc'
-alias wdb='clipkey MySQL'
+# alias tok='clipkey Token.asc'
