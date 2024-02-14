a52b0eda631e260db53390303720e6d073915154
deftclaw <eeccher@cppwind.com>
2024-02-14 13:01:02 -0700

    Update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,8 @@
 # System-Wide Aliases : 
+alias EAL='sudo $EDITOR /etc/profile.d/aliases.sh'
 alias SVC='sudo systemctl'
+alias SVC='sudo systemctl'
+alias bat='batcat'
 alias cat='$PAGER'
 alias l='eza -F'
 alias la='eza -Fa'
@@ -7,12 +10,16 @@ alias ll='eza -Fal'
 alias ls='eza -F'
 alias lsblk='lsblk -o LABEL,NAME,FSUSED,FSUSE%,FSAVAIL,SIZE,TYPE,FSTYPE,MOUNTPOINTS,UUID'
 alias lsu='ls /media/usb/0'
+alias nf='$EDITOR $XP/'
 alias pd='pushd'
 alias q='exit'
 alias rm='rm -v'
-alias rs='reset ; printf "\033[1B\033[38;5;30m\e[3m     # Shell Reloaded\e[0m\033[0m\033[22D\033[2A" ; source /etc/profile'                # Reload the profile efficiently
+alias rs='reset ; printf "\033[1B\033[38;5;30m\e[3m     # Shell Reloaded\e[0m\033[0m\033[22D\033[2A" ; source /etc/profile'  # Reload the profile efficiently
 alias sv='sudo XDG_CONFIG_HOME=$XDG_CONFIG_HOME nvim' # Edit as root with config
+alias tal='eza -FaglTL'
+alias tree='eza -FT'
 alias xo='xdg-open'
+alias xrg='xargs -I %'
 
 # Profile things
 alias ea='nf aliases'
@@ -25,42 +32,6 @@ alias sv='sudo XDG_CONFIG_HOME=$XDG_CONFIG_HOME nvim' # Edit as root with config
 alias tal='exa -FaglTL'
 alias tree='exa -FT'
 alias xrg='xargs -I %'
-
-# Persisant aliases for every bash session : 
-  alias bat='batcat'
-  alias EAL='sudo $EDITOR /etc/profile.d/aliases.sh'
-  alias SVC='sudo systemctl'
-  alias la='exa -Fa'
-  alias ll='exa -Fal'
-  alias ls='exa -F'
-  alias lsblk='lsblk -o LABEL,NAME,FSTYPE,RO,TYPE,FSUSED,SIZE,MOUNTPOINT'
-  alias nf='$EDITOR $XP/'
-  alias pd='pushd'
-  alias q='exit'
-  alias rm='rm -v'
-  alias rs='reset ; source /etc/profile'                # Reload the profile efficiently
-  alias sv='sudo XDG_CONFIG_HOME=$XDG_CONFIG_HOME nvim' # Edit as root with config
-  alias tal='exa -FaglTL'
-  alias tree='exa -FT'
-  alias xrg='xargs -I %'
-
-# Persisant aliases for every bash session : 
-  alias bat='batcat'
-  alias EAL='sudo $EDITOR /etc/profile.d/aliases.sh'
-  alias SVC='sudo systemctl'
-  alias la='exa -Fa'
-  alias ll='exa -Fal'
-  alias ls='exa -F'
-  alias lsblk='lsblk -o LABEL,NAME,FSTYPE,RO,TYPE,FSUSED,SIZE,MOUNTPOINT'
-  alias nf='$EDITOR $XP/'
-  alias pd='pushd'
-  alias q='exit'
-  alias rm='rm -v'
-  alias rs='clear ; printf "\033[1B\033[38;5;30m\e[3m     # Shell Reloaded\e[0m\033[0m\033[20D\033[2A" ; source /etc/profile' # Reload the profile efficiently
-  alias sv='sudo XDG_CONFIG_HOME=$XDG_CONFIG_HOME nvim' # Edit as root with config
-  alias tal='exa -FaglTL'
-  alias tree='exa -FT'
-  alias xrg='xargs -I %'
 
 # Quick move
 alias ..='cd ..'
