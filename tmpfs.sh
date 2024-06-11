14ff661f5272ceef571f5d83cf844beb0f54247a
deftclaw <eeccher@cppwind.com>
2024-06-11 11:19:48 -0600

    Fix creating multiple tmp dirs, make sure TMP does not have trailing /
    Signed-off-by: deftclaw <eeccher@cppwind.com>

--- tmpfs.sh
+++ tmpfs.sh
@@ -1,6 +1,6 @@
 # Create default RAM downloads folder, and personal-permission tmpfs : 
 [[ -d /tmp/dwn ]] || mkdir /tmp/dwn
 
-tmpfs=$(ls -F /tmp|ag '\/$'|ag 'tmp\.')
+tmpfs=$(/bin/ls -F /tmp|ag '\/$'|ag 'tmp\.')
 
-[[ -n $tmpfs ]] && export TMP=/tmp/${tmpfs[0]} || export TMP=$(mktemp -d)
+[[ -n $tmpfs ]] && export TMP=`echo /tmp/${tmpfs[0]}|sed 's/\/$//g'` || export TMP=$(mktemp -d)
