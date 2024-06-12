d3b8a4f8e664b3ab880ce0acb2e187e60d9bfbb8
deftclaw <deftclaw@briskbear.rocks>
2024-06-11 18:16:00 -0600

    remove neovim, using binary instead, use 'bat' on arch, and cat debian, because debian sucks

--- environment.sh
+++ environment.sh
@@ -1,7 +1,7 @@
 # Global Environment : 
 ## Default Tools
 export EDITOR=/usr/bin/nvim
-export PAGER=/usr/bin/bat
+export PAGER=/usr/bin/cat
 
 ## XDG Config
 export XDG_CONFIG_HOME=$HOME/.local/etc/xdg
