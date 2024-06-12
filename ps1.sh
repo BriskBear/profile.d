f6d7ca872cfeadc2f97eb4e0c3ab125fe3b8e736
deftclaw <deftclaw@briskbear.rocks>
2024-06-11 22:05:12 -0600

    add code-minimap, dynamic prompt-icon

--- ps1.sh
+++ ps1.sh
@@ -3,4 +3,5 @@ PS1='\u'                                   #  Display Username
 PS1+='\[$( errname $? )\]'                 #  Display ]|[ or error code, random-color hostname
 PS1+=':\w'                                 #  Display current directory
 PS1+='$(__git_ps1 " (%s)" &2> /dev/null) ' #  Git branch and status
-PS1+='\n\[$( glyph " " )\]'               #  Display uniform-length prompt-icon
+# Display uniform-length prompt-icon, # for root else $
+[[ $UID == 0 ]] && PS1+='\n\[$( glyph " " )\]' || PS1+='\n\[$( glyph " " )\]'
