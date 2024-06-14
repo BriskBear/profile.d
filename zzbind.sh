e740fd59c2b753166c20f889b0ace574d5f61bf1
deftclaw <deftclaw@briskbear.rocks>
2024-06-13 21:24:40 -0600

    append zzbind, add tmpfs fixes, install non-root fix

--- zzbind.sh
+++ zzbind.sh
@@ -1,3 +1,13 @@
 # CMDline keyboard shortcuts : 
 bind -x '"\C-l": "clear -x;date|lolcat"' 2>/dev/null
 bind '"kj": "\e"' 2>/dev/null
+bind -x '"\C-p": "git stash --all; git pull"'
+bind -x '"\C-r": "restart-shell"'
+
+# Surrounding Elements
+bind '"``":"`kjmha`kj`ha"'
+bind '"\"\"":"\"kjmha\"kj`ha"'
+bind '"\(\(":"\(kjmha\)kj`ha"'
+bind '"\{\{":"\{kjmha\}kj`ha"'
+bind '"\[\[":"\[kjmha\]kj`ha"'
+bind '"\[\[ ":"\[ kjmha \]kj`ha"'
