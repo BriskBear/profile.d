fac4b3c1b711030452595b0c4ef1ce663b7638d1
Elijah <57646863+deftclaw@users.noreply.github.com>
2024-09-05 11:54:22 -0600

    Update environment.sh
    `history` file size to 10000 lines

--- environment.sh
+++ environment.sh
@@ -3,6 +3,10 @@
 export EDITOR=/usr/bin/nvim
 export PAGER=bat
 
+## History
+export HISTSIZE=10000
+export HISTFILESIZE=10000
+
 ## XDG Config
 export XDG_CONFIG_HOME=$HOME/.local/etc/xdg
 export XDG_CONFIG_PROFILE=$HOME/.local/etc/profile.d
