1c2eb93fd4c8a1d7ef077803ac1902930a739903
Elijah <57646863+deftclaw@users.noreply.github.com>
2024-10-07 19:42:21 -0600

    Update environment.sh
    don need no xdg

--- environment.sh
+++ environment.sh
@@ -8,7 +8,7 @@ export HISTSIZE=10000
 export HISTFILESIZE=10000
 
 ## XDG Config
-export XDG_CONFIG_HOME=$HOME/.local/etc/xdg
+export XDG_CONFIG_HOME=$HOME/.local/etc
 export XDG_CONFIG_PROFILE=$HOME/.local/etc/profile.d
 
 ## XDG Shortcuts
