8ab125417c691978b31634a6e14926faa43f30ec
deftclaw <deftclaw@briskbear.rocks>
2025-03-08 08:03:35 -0700

    Make $tmpfs a list instead of a single string

--- tmpfs.sh
+++ tmpfs.sh
@@ -1,6 +1,6 @@
 # Create default RAM downloads folder, and personal-permission tmpfs : 
 [[ -d /tmp/dwn ]] || mkdir /tmp/dwn
 
-tmpfs=$(/usr/bin/ls -F /tmp|ag '\/$'|ag 'tmp\.')
+tmpfs=(`/usr/bin/ls -F /tmp|ag '\/$'|ag 'tmp\.'`)
 
 [[ -n $tmpfs ]] && export TMP=`echo /tmp/${tmpfs[0]}|sed 's/\/$//g'` || export TMP=$(mktemp -d)
