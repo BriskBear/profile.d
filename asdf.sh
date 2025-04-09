0bf23033b5c507348176796185453fad30ae2ce6
deftclaw <deftclaw@briskbear.rocks>
2025-04-09 11:00:44 -0600

    fix dropped "`"

--- asdf.sh
+++ asdf.sh
@@ -11,7 +11,7 @@ function asdf_install_latest() {
   # Add plugin if without
   [[ `asdf list ${lang}` ]] || asdf plugin add ${lang}
 
-  list=(`asdf list all ${lang}|grep -E '^[0-9]+\.[0-9]+\.[0-9]+$')
+  list=(`asdf list all ${lang}|grep -E '^[0-9]+\.[0-9]+\.[0-9]+$'`)
 
   asdf install ${lang} ${list[-1]}
   asdf set -u  ${lang} ${list[-1]}
