b9bbf344fd4a80525fd08a8f94abead7a00375de
deftclaw <deftclaw@briskbear.rocks>
2025-04-09 10:41:51 -0600

    Clear timestamp, use grep so not relying on ag

--- asdf.sh
+++ asdf.sh
@@ -1,4 +1,4 @@
-# Source essential asdf functions : 1744064326
+# Source essential asdf functions : 
 export ASDF_DATA_DIR="$HOME/.local/share/asdf"
 
 [[ $PATH =~ $ASDF_DATA_DIR/shims ]] || export PATH=$PATH:$ASDF_DATA_DIR/shims
@@ -11,7 +11,7 @@ function asdf_install_latest() {
   # Add plugin if without
   [[ `asdf list ${lang}` ]] || asdf plugin add ${lang}
 
-  list=(`asdf list all ${lang}|ag '^\d+\.\d+\.\d+$'`)
+  list=(`asdf list all ${lang}|grep -E '^[0-9]+\.[0-9]+\.[0-9]+$')
 
   asdf install ${lang} ${list[-1]}
   asdf set -u  ${lang} ${list[-1]}
