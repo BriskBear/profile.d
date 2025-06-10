c0223d0fff3d59052e1947218624655e05e54c4f
Elijah Eccher <eeccher@cppwind.com>
2025-06-10 14:50:09 -0600

    Update PS1 line column count, fix mis-match

--- ps1.sh
+++ ps1.sh
@@ -1,7 +1,7 @@
 # Set the shell prompt : 
 PS1='\u'                                   #  Display Username
-PS1+='\[$( errname $? )\]'                 #  Display ]|[ or error code, random-color hostname
+PS1+='$( errname $? )'                 #  Display ]|[ or error code, random-color hostname
 PS1+=':\w'                                 #  Display current directory
 PS1+='$(__git_ps1 " (%s)" &2> /dev/null) ' #  Git branch and status
 # Display uniform-length prompt-icon, # for root else $
-[[ $UID == 0 ]] && PS1+='\n\[$( glyph " " )\]' || PS1+='\n\[$( glyph " " )\]'
+[[ $UID == 0 ]] && PS1+='\n$( glyph " " )' || PS1+='\n$( glyph " " )'
