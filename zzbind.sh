37052d66f90fdd4133819b7d63703b9b26abb5f6
Elijah Eccher <eeccher@cppwind.com>
2025-06-10 15:23:09 -0600

    fix install script and tweak binds

--- zzbind.sh
+++ zzbind.sh
@@ -1,13 +1,11 @@
-# CMDline keyboard shortcuts : 
+# CMDline keyboard shortcuts : 1749589439
 bind -x '"\C-l": "clear -x;date|lolcat"' 2>/dev/null
 bind '"kj": "\e"' 2>/dev/null
-bind -x '"\C-p": "git stash --all; git pull"'
-bind -x '"\C-r": "restart-shell"'
 
-# Surrounding Elements
+# Surround Elements
 bind '"``":"`kjmha`kj`ha"'
-bind '"\"\"":"\"kjmha\"kj`ha"'
+bind '"\"\""":"\"kjmha\"kj`ha"'
 bind '"\(\(":"\(kjmha\)kj`ha"'
 bind '"\{\{":"\{kjmha\}kj`ha"'
-bind '"\[\[":"\[kjmha\]kj`ha"'
-bind '"\[\[ ":"\[ kjmha \]kj`ha"'
+bind '"\[\[":"\[\]\eha"'
+bind '"\[ ":"\[\]\eha  \eha"'
