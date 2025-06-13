792ecc08a807a0c396e296dda862d89b281f6ea6
dc <deftclaw@briskbear.rocks>
2025-06-12 19:19:29 -0600

    display colors on background for better view

--- color_sample.sh
+++ color_sample.sh
@@ -1,14 +1,15 @@
 # sample terminal 256-colors : 
 function color_sample() {
   numbers=( $(seq 0 256) )
-  lead="\u001b[38;5;"
+  lead="\u001b[38;5;16m\u001b[1;\u001b[48;5;"
   nc="\u001b[0m"
 
   for c in ${numbers[@]}
   do
-    printf "$lead${c}m$c$nc"
+    printf "$lead${c}m"
     [[ $c -lt 10 ]] && printf " "
+    printf "$c"
     [[ $c -lt 100 ]] && printf " "
-    [[ $( echo "$c % 16"|bc ) -eq 0 ]] && printf "\n" || printf " "
+    [[ $( echo "$c % 16"|bc ) -eq 0 ]] && printf "$nc\n" || printf "$nc "
   done
 }
