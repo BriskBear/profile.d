0fe0abbb809a254fa74c26561861e259ee8a6a44
deftclaw <deftclaw@briskbear.rocks>
2025-07-03 22:43:22 -0600

    package_manager: ubuntu update does not accept -y flag

--- package_manager/ubuntu.sh
+++ package_manager/ubuntu.sh
@@ -4,7 +4,7 @@
 export package_mgr=`which nala 2>/dev/null || which apt-get 2>/dev/null`
 
 function ubuntu-update() {
-   $doas $package_mgr update -y
+   $doas $package_mgr update
    $doas $package_mgr upgrade -y
 }
 
