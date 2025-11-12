6945ed74326161e13eece33b91ce89542d7e9eb9
deftclaw <deftclaw@briskbear.rocks>
2025-11-12 11:15:37 -0700

    Add completion to tmux-attach - and it's alias - 'ta'

--- tmux.sh
+++ tmux.sh
@@ -1,11 +1,20 @@
-# TMUX Shortcuts, Attach creates the session if it does not exist : 
+# TMUX Shortcuts, Attach creates the session if it does not exist : 1762971281
 tmux-attach() {
   session="$1"
   
-  [[ ! -n $session ]] \
+  [[ ! -n $session ]]          \
     && tmux attach &>/dev/null \
     || ( tmux attach -t "$session" &>/dev/null || tmux new-session -s "$session" &>/dev/null)
 }
 
+function _tmux-attach_completion() {
+  SESSIONS=(`tmux ls | awk -F':' '{print $1}'`) 
+
+  COMPREPLY=(`compgen -W "${SESSIONS[*]}" "${COMP_WORDS[1]}"`)
+}
+
 alias ta='tmux-attach'
 alias txl='tmux ls'
+
+complete -F _tmux-attach_completion tmux-attach
+complete -F _tmux-attach_completion ta
