74919f15b53dc7149acb3f84a5bcc57606ff6a50
deftclaw <deftclaw@briskbear.rocks>
2025-11-12 11:00:34 -0700

    Actually update the function's name ; add completion to the function and it's alias

--- edit_profile.sh
+++ edit_profile.sh
@@ -1,8 +1,19 @@
 # Create a new built-in bash function : 
-function new-function() {
+function edit-profile() {
   [[ $# == 1 ]] \
-    && nvim +3 $HOME/.local/etc/profile.d/$1.sh \
+    && nvim +3 $HOME/.local/etc/profile.d/$1 \
     || nvim +5 $HOME/.local/etc/profile.d/
 }
 
-alias nf='new-function'
+function _edit-profile_completions() {
+  PROFILES=(`find $XP | ag '\.sh$' | sed "s:$HOME/.local/etc/profile.d/::"`) 
+
+  if [[ ${COMP_WORDS[0]} =~ ':' ]]
+  then COMPREPLY=($(compgen -W "${PROFILES[*]}" "${COMP_WORDS[2]}"))
+  else COMPREPLY=($(compgen -W "${PROFILES[*]}" "${COMP_WORDS[1]}"))
+  fi
+}
+
+complete -F _edit-profile_completions edit-profile
+alias :p='edit-profile'
+complete -F _edit-profile_completions :p
