61653e5547f1356c645495de7720ceb708674903
deftclaw <deftclaw@briskbear.rocks>
2026-05-11 10:08:56 -0600

    fix tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,15 +1,20 @@
 # TMUX Shortcuts, Attach creates the session if it does not exist : 
+function tmux-attach() {
   session="$1"
   
-  [[ ! -n $session ]]          \
-    && tmux attach &>/dev/null \
-    || ( tmux attach -t "$session" &>/dev/null || tmux new-session -s "$session" &>/dev/null)
+  [[ -z $session ]]                \
+    && ( tmux attach &>/dev/null ) \
+    || ( tmux new -As "${session:-`caps-host`}" )
 }
 
 function _tmux-attach_completion() {
   SESSIONS=(`tmux ls | awk -F':' '{print $1}'`) 
 
   COMPREPLY=(`compgen -W "${SESSIONS[*]}" "${COMP_WORDS[1]}"`)
+}
+
+function caps-host() {
+	echo "$HOSTNAME" | tr [:lower:] [:upper:] | sed -e 's/I7//g' -e 's/10900K//g'
 }
 
 alias ta='tmux-attach'
